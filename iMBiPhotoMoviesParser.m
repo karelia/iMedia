5a14249a2ead158a147bfe234adce04e9b5564fc
Greg Hulands <ghulands@me.com>
2006-06-07 04:53:13 +0000

    Check to see if the keyword maps okay before adding it

--- iMBiPhotoMoviesParser.m
+++ iMBiPhotoMoviesParser.m
@@ -179,7 +179,10 @@ Please send fixes to
 				
 				while (keywordKey = [keywordEnum nextObject]) {
 					NSString *actualKeyword = [keywordMap objectForKey:keywordKey];
-					[realKeywords addObject:actualKeyword];
+					if (actualKeyword)
+					{
+						[realKeywords addObject:actualKeyword];
+					}
 				}
 				
 				NSMutableDictionary *mutatedKeywordRecord = [NSMutableDictionary dictionaryWithDictionary:imageRecord];
