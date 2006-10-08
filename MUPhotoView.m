71d43649d84b123a8cb0fc1b21d5583625d8ebb6
Greg Hulands <ghulands@me.com>
2006-10-08 01:08:58 +0000

    creates correct range

--- MUPhotoView.m
+++ MUPhotoView.m
@@ -1620,19 +1620,14 @@ static NSImage *_badge = nil;
 
 - (NSRange)photoIndexRangeForRect:(NSRect)rect
 {
-	unsigned photoCount = [self photoCount];
-	if (!photoCount)
-		return NSMakeRange(NSNotFound, 0);
-
-    unsigned start = [self photoIndexForPoint:rect.origin];
-	if (start >= photoCount)
-		return NSMakeRange(NSNotFound, 0);
-		
-	unsigned finish = [self photoIndexForPoint:NSMakePoint(NSMaxX(rect), NSMaxY(rect))];
-	if (finish >= photoCount)
-		finish = photoCount - 1;
-    
-	return NSMakeRange(start, (finish + 1) - start);
+    unsigned	start = [self photoIndexForPoint:rect.origin];
+	unsigned	finish = [self photoIndexForPoint:NSMakePoint(NSMaxX(rect), NSMaxY(rect))];
+	unsigned	picCount = [self photoCount];
+	
+    if (finish >= picCount)
+        finish = picCount;
+	
+	return NSMakeRange(start, finish-start);	
 }
 
 - (NSRect)gridRectForIndex:(unsigned)index
