a71734d86defea328f5abfcb65860aa0828fec3c
Greg Hulands <ghulands@me.com>
2007-01-13 05:00:25 +0000

    updated epeg

---
+++ Epeg.framework/Versions/A/Headers/EpegWrapper.h
@@ -0,0 +1,20 @@
+//
+//  EpegWrapper.h
+//  Epeg
+//
+//  Created by Marc Liyanage on Fri Jan 16 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface EpegWrapper : NSObject 
+{
+	
+}
+
++ (NSImage *)imageWithPath:(NSString *)path boundingBox:(NSSize)boundingBox;
+
+
+@end
