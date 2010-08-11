a73f432eb1c8fc225fc6da6a2f60ffb1279ec34d
peter@baumgartner.com@0a3f77c4-9235-0410-95f4-d138c4647c51 <peter@baumgartner.com@0a3f77c4-9235-0410-95f4-d138c4647c51>
2010-08-11 09:42:21 +0000

    Added nil checks to guard against exceptions (which can wreak havoc during app startup)
    --HG--
    extra : convert_revision : svn%3A0a3f77c4-9235-0410-95f4-d138c4647c51/trunk%401088

--- IMBConfig.m
+++ IMBConfig.m
@@ -174,7 +174,7 @@ static NSString* sIMBEditorAppPathsKey = @"editorAppPaths";
 + (void) setViewerApp:(NSString*)inAppPath forMediaType:(NSString*)inMediaType
 {
 	NSMutableDictionary* viewerAppPaths = [NSMutableDictionary dictionaryWithDictionary:[self prefsValueForKey:sIMBViewerAppPathsKey]];
-	[viewerAppPaths setObject:inAppPath forKey:inMediaType];
+	if (inAppPath) [viewerAppPaths setObject:inAppPath forKey:inMediaType];
 	[self setPrefsValue:viewerAppPaths forKey:sIMBViewerAppPathsKey];
 }
 
@@ -194,7 +194,7 @@ static NSString* sIMBEditorAppPathsKey = @"editorAppPaths";
 + (void) setEditorApp:(NSString*)inAppPath forMediaType:(NSString*)inMediaType
 {
 	NSMutableDictionary* editorAppPaths = [NSMutableDictionary dictionaryWithDictionary:[self prefsValueForKey:sIMBEditorAppPathsKey]];
-	[editorAppPaths setObject:inAppPath forKey:inMediaType];
+	if (inAppPath) [editorAppPaths setObject:inAppPath forKey:inMediaType];
 	[self setPrefsValue:editorAppPaths forKey:sIMBEditorAppPathsKey];
 }
 
