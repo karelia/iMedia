d650cdcc6b4f273fddad6ff78108af7135cda90a
dan94501@0a3f77c4-9235-0410-95f4-d138c4647c51 <dan94501@0a3f77c4-9235-0410-95f4-d138c4647c51>
2010-09-15 21:19:37 +0000

    quiet down dragging
    --HG--
    extra : convert_revision : svn%3A0a3f77c4-9235-0410-95f4-d138c4647c51/trunk%401175

--- IMBImageBrowserView.m
+++ IMBImageBrowserView.m
@@ -345,7 +345,7 @@ enum IMBMouseOperation
 
 - (void) draggedImage:(NSImage*)inImage endedAt:(NSPoint)inScreenPoint operation:(NSDragOperation)inOperation
 {
-	NSLog(@"- (void) draggedImage:(NSImage*)inImage endedAt:(NSPoint)inScreenPoint operation:(NSDragOperation)inOperation");
+	// NSLog(@"- (void) draggedImage:(NSImage*)inImage endedAt:(NSPoint)inScreenPoint operation:(NSDragOperation)inOperation");
 	[self.delegate draggedImage:inImage endedAt:inScreenPoint operation:inOperation];
 }
 
