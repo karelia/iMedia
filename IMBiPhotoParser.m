6c6d70cbb3b4cddfdf7c09e4919336a9aca1d2db
Dan Wood <dwood@karelia.com>
2010-11-11 20:01:02 +0000

    From Jörg Jacobsen: First step at getting iPhoto events

--- IMBiPhotoParser.m
+++ IMBiPhotoParser.m
@@ -67,11 +67,22 @@
 
 //----------------------------------------------------------------------------------------------------------------------
 
+// Since we will add an events node to the list of albums we have to create an id for it
+// that is unique throughout this library. The one chosen below is very, very likely to be.
+// Actually checking uniqueness would require us to traverse different arrays and would
+// not necessarily be future proof (e.g. if we'd once add 'faces' to the browser
+// we'd also have to check against faces ids).
+
+#define EVENTS_NODE_ID UINT_MAX-4811	// Very, very unlikely this not to be unique throughout library
+
+//----------------------------------------------------------------------------------------------------------------------
 
 #pragma mark 
 
 @interface IMBiPhotoParser ()
 
+- (void) addEventsToAlbumsInLibrary:(NSMutableDictionary*)dict;
+- (IMBNode*) eventsNodeInNode:(IMBNode*) inNode;
 - (NSString*) identifierWithAlbumId:(NSNumber*)inAlbumId;
 - (NSString*) iPhotoMediaType;
 - (BOOL) shouldUseAlbumType:(NSString*)inAlbumType;
@@ -305,15 +316,39 @@
 	[self addSubNodesToNode:inNode albums:albums images:images]; 
 	[self populateNode:inNode albums:albums images:images]; 
 
-	// If we are populating the root nodes, then also populate the "Photos" node (first subnode) and mirror its
+	// If we are populating the root nodes, then also populate the "Photos" node (second subnode) and mirror its
 	// objects array into the objects array of the root node. Please note that this is non-standard parser behavior,
 	// which is implemented here, to achieve the desired "feel" in the browser...
 	
-	if (inNode.isRootNode && [inNode.subNodes count]>0)
+	// Also pre-populate events node so that objects can be shared between events node and photos node
+	// (This is assuming that the union of all event objects is just about all objects)
+	
+	NSArray* subNodes = inNode.subNodes;
+	
+	if (inNode.isRootNode && [subNodes count] > 0)
 	{
-		IMBNode* photosNode = [inNode.subNodes objectAtIndex:0];
+		IMBNode* eventsNode = [self eventsNodeInNode:inNode];
+		
+		// Depending on whether there is an events node or not and
+		// depending on where it's located
+		// we find photos node at a different index
+		int photosNodeIndex = eventsNode &&
+							  eventsNode == [subNodes objectAtIndex:0] &&
+							  [subNodes count] > 1 ? 1 : 0;
+
+		IMBNode* photosNode = [subNodes objectAtIndex:photosNodeIndex];
 		[self populateNode:photosNode options:inOptions error:outError];
 		inNode.objects = photosNode.objects;
+		
+		// Pre-populate events node with the same objects that photos node has
+		
+		if (eventsNode != nil && eventsNode != photosNode)		// which should really be the case
+		{
+			eventsNode.objects = photosNode.objects;
+			
+			// events node is not considered populated if subnodes aren't instantiated yet
+			[self addSubNodesToNode:eventsNode albums:albums images:images];
+		}
 	}
 	
 	if (outError) *outError = error;
@@ -472,7 +507,9 @@
 		
 		if (_plist == nil)
 		{
-			NSDictionary* dict = [NSDictionary dictionaryWithContentsOfFile:(NSString*)self.mediaSource];
+			// Since we want to add events to the list of albums we will need
+			// to modify the album data dictionary (see further down below)
+			NSMutableDictionary* dict = [NSMutableDictionary dictionaryWithContentsOfFile:(NSString*)self.mediaSource];
 			
 			// WORKAROUND
 			if (!dict || 0 == dict.count)	// unable to read. possibly due to unencoded '&'.  rdar://7469235
@@ -488,9 +525,14 @@
                                                                             options:NSXMLDocumentTidyXML error:&e];
                         dict = [NSPropertyListSerialization
                               propertyListFromData:[xmlDoc XMLData]
-                              mutabilityOption:NSPropertyListImmutable
+                              mutabilityOption:0					// Apple doc: The opt parameter is currently unused and should be set to 0.
                               format:NULL errorDescription:&eString];
                         [xmlDoc release];
+						
+						// the assignment to 'dict' in the code above yields
+						// a mutable dictionary as this code snippet would reveal:
+						// Class dictClass = [dict classForCoder];
+						// NSLog(@"Dictionary class: %@", [dictClass description]);
                     }
                     @catch(NSException *e)
                     {
@@ -503,8 +545,15 @@
 			if (!dict || 0 == dict.count)
 			{
 				//	If there is an AlbumData.xml file, there should be something inside!
-				NSLog (@"The iPhoto AlbumData.xml file seams to be empty. This is an unhealthy condition!");
+				NSLog (@"The iPhoto AlbumData.xml file seems to be empty. This is an unhealthy condition!");
 			}
+			
+			// Since this parser confines itself to deal with the "List of Albums" only
+			// we add an events node to the album list to incorporate events in the browser.
+			// This is why we need a mutable library dictionary.
+						
+			[self addEventsToAlbumsInLibrary:dict];
+			
 			self.plist = dict;
 			self.modificationDate = modificationDate;
 		}
@@ -513,6 +562,121 @@
 	}
 	
 	return plist;
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// iPhoto does not include events in the album list. To let events also be shown
+// in the browser we let events (aka rolls) pose as albums in the album list
+// an let them be children of an 'events' node that we also add to the album list.
+
+- (void) addEventsToAlbumsInLibrary:(NSMutableDictionary*) dict
+{	
+	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+	
+	// To insert events into the album list we have to re-create it mutable style
+	NSMutableArray *newAlbumList = [[NSMutableArray alloc] init];
+	
+	// 1. We want the events node to be first in the list
+	// Create and append parent object for all events.
+	
+	NSDictionary* events;
+	NSNumber *eventsId = [NSNumber numberWithUnsignedInt:EVENTS_NODE_ID];
+	NSString *eventsName = NSLocalizedStringWithDefaultValue(@"IMB.iPhotoParser.events", nil, IMBBundle(), @"Events", @"Events node shown in iPhoto library");
+	
+	@try {
+		NSArray* eventList = [dict objectForKey:@"List of Rolls"];		
+		NSAssert( eventList != nil, @"'List of Rolls' key not found in iPhoto album data property list");
+		
+		NSArray* oldAlbumList = [dict objectForKey:@"List of Albums"];		
+		NSAssert( oldAlbumList != nil, @"'List of Albums' key not found in iPhoto album data property list");
+		NSAssert( [oldAlbumList count] > 0, @"No albums in list of albums");
+		
+		NSDictionary *photosDict = [oldAlbumList objectAtIndex:0];	// Photos node
+		
+		id keyList = [photosDict objectForKey:@"KeyList"];
+		NSAssert( keyList != nil, @"'KeyList' key not found in photos dictionary in iPhoto album data property list");
+		
+		events = [[NSDictionary alloc] initWithObjectsAndKeys:
+				  eventsId,			@"AlbumId",
+				  eventsName,		@"AlbumName",
+				  @"Events",		@"Album Type",
+				  keyList,			@"KeyList", nil];   // populate events with same media objects as photos album
+		
+		[newAlbumList addObject:events];
+		
+		// 2. Append all regular albums. Note that the photos album will now be at index 1 of the list
+		[newAlbumList addObjectsFromArray:oldAlbumList];
+		
+		// 3. Create and append events to album list and make each event behave like a proper album list citizen
+		
+		id rollId, rollName;
+		
+		for	(NSDictionary *eventDict in eventList) {
+			
+			rollId   = [eventDict objectForKey:@"RollID"];
+			rollName = [eventDict objectForKey:@"RollName"];
+			keyList  = [eventDict objectForKey:@"KeyList"];
+			NSAssert( rollId != nil, @"'RollID' key not found in event dictionary in iPhoto album data property list");
+			NSAssert( rollName != nil, @"'RollName' key not found in event dictionary in iPhoto album data property list");
+			NSAssert( keyList != nil, @"'KeyList' key not found in event dictionary in iPhoto album data property list");
+			
+			
+			NSDictionary *albumDict = [[NSDictionary alloc] initWithObjectsAndKeys:
+									   rollId,		@"AlbumId",
+									   rollName,	@"AlbumName",
+									   @"Event",	@"Album Type",
+									   eventsId,	@"Parent",
+									   keyList,		@"KeyList", nil];
+			
+			[newAlbumList addObject:albumDict];
+			[albumDict release];
+		}
+		
+		// Finally, replace the old albums array.
+		[dict setValue:newAlbumList forKey:@"List of Albums"];
+		[newAlbumList release];
+	}
+	@catch (NSException * e) {
+		// Some expected key not present in album data property list
+		NSLog(@"%@", [e description]);
+	}
+	@finally {
+		if (events) [events release];
+	}
+	
+	[pool drain];
+}
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Returns the events node that should be subnode of our root node.
+// Returns nil if there is none.
+
+- (IMBNode*) eventsNodeInNode:(IMBNode*) inNode {
+	
+	IMBNode* eventsNode= nil;
+	
+	if (inNode.isRootNode && [inNode.subNodes count]>0) {
+		
+		NSNumber* eventsId = [NSNumber numberWithUnsignedInt:EVENTS_NODE_ID];
+		NSString* eventsIdWithAlbumId = [self identifierWithAlbumId:eventsId];
+		
+		// We should find the events node at index 0 but this logic is more bullet proof.
+		
+		for (IMBNode* node in inNode.subNodes) {
+			
+			if ([node.identifier isEqualToString:eventsIdWithAlbumId]) {
+				eventsNode = node;
+				break;
+			}
+		}
+	}
+	
+	return eventsNode;
 }
 
 
@@ -642,7 +806,7 @@
 
 - (BOOL) isLeafAlbumType:(NSString*)inType
 {
-	return ![inType isEqualToString:@"Folder"];
+	return ![inType isEqualToString:@"Folder"] && ![inType isEqualToString:@"Events"];
 }
 
 
@@ -682,6 +846,11 @@
 			albumNode.name = albumName;
 			albumNode.mediaSource = self.mediaSource;
 			albumNode.parser = self;
+			
+			// Keep a ref to the album dictionary for later use when we populate this node
+			// so we don't have to loop through the whole album list again to find it.
+			
+			albumNode.attributes = albumDict;
 
 			// Set the node's identifier. This is needed later to link it to the correct parent node. Please note 
 			// that older versions of iPhoto didn't have AlbumId, so we are generating fake AlbumIds in this case
@@ -734,58 +903,49 @@
 	NSMutableArray* objects = (NSMutableArray*) inNode.objects;
 	if (objects == nil) inNode.objects = objects = [NSMutableArray array];
 
-	// Look for the correct album in the iPhoto XML plist. Once we find it, populate the node with IMBVisualObjects
-	// for each image in this album...
+	// Populate the node with IMBVisualObjects for each image in the album
 	
 	NSUInteger index = 0;
 	Class objectClass = [self objectClass];
 	
-	for (NSDictionary* albumDict in inAlbums)
+	// We saved a reference to the album dictionary when this node was created
+	// (ivar 'attributes') and now happily reuse it to save an outer loop (over album list) here.
+	
+	NSDictionary* albumDict = inNode.attributes;	
+	NSArray* imageKeys = [albumDict objectForKey:@"KeyList"];
+	
+	for (NSString* key in imageKeys)
 	{
-		NSAutoreleasePool* pool1 = [[NSAutoreleasePool alloc] init];
-		NSNumber* albumId = [albumDict objectForKey:@"AlbumId"];
-		NSString* albumIdentifier = albumId ? [self identifierWithAlbumId:albumId] : [self identifierForPath:@"/"];
+		NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+		NSDictionary* imageDict = [inImages objectForKey:key];
 		
-		if ([inNode.identifier isEqualToString:albumIdentifier])
+		if ([self shouldUseObject:imageDict])
 		{
-			NSArray* imageKeys = [albumDict objectForKey:@"KeyList"];
-
-			for (NSString* key in imageKeys)
+			NSString* path = [imageDict objectForKey:@"ImagePath"];
+			NSString* name = [imageDict objectForKey:@"Caption"];
+			if ([name isEqualToString:@""])
 			{
-				NSAutoreleasePool* pool2 = [[NSAutoreleasePool alloc] init];
-				NSDictionary* imageDict = [inImages objectForKey:key];
-				
-				if ([self shouldUseObject:imageDict])
-				{
-					NSString* path = [imageDict objectForKey:@"ImagePath"];
-					NSString* name = [imageDict objectForKey:@"Caption"];
-					if ([name isEqualToString:@""])
-					{
-						name = [[path lastPathComponent] stringByDeletingPathExtension];	// fallback to filename
-					}
-					
-					IMBObject* object = [[objectClass alloc] init];
-					[objects addObject:object];
-					[object release];
-
-					object.location = (id)path;
-					object.name = name;
-					object.preliminaryMetadata = imageDict;	// This metadata from the XML file is available immediately
-					object.metadata = nil;					// Build lazily when needed (takes longer)
-					object.metadataDescription = nil;		// Build lazily when needed (takes longer)
-					object.parser = self;
-					object.index = index++;
-					
-					object.imageLocation = [self imageLocationForObject:imageDict];
-					object.imageRepresentationType = [self requestedImageRepresentationType];
-					object.imageRepresentation = nil;
-				}
-				
-				[pool2 drain];
+				name = [[path lastPathComponent] stringByDeletingPathExtension];	// fallback to filename
 			}
+			
+			IMBObject* object = [[objectClass alloc] init];
+			[objects addObject:object];
+			[object release];
+			
+			object.location = (id)path;
+			object.name = name;
+			object.preliminaryMetadata = imageDict;	// This metadata from the XML file is available immediately
+			object.metadata = nil;					// Build lazily when needed (takes longer)
+			object.metadataDescription = nil;		// Build lazily when needed (takes longer)
+			object.parser = self;
+			object.index = index++;
+			
+			object.imageLocation = [self imageLocationForObject:imageDict];
+			object.imageRepresentationType = [self requestedImageRepresentationType];
+			object.imageRepresentation = nil;
 		}
 		
-		[pool1 drain];
+		[pool drain];
 	}
 }
 
