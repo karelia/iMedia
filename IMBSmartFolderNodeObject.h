b0fb989631f435788833ce6952c675bcd3fe0051
Dan Wood <dwood@karelia.com>
2011-01-05 00:13:17 +0000

    new object with a smart folder icon for use by flickr parser and potentially others with smart folders rather than folders

---
+++ IMBSmartFolderNodeObject.h
@@ -0,0 +1,18 @@
+//
+//  IMBSmartFolderNodeObject.h
+//  iMedia
+//
+//  Created by Dan Wood on 1/4/11.
+//  Copyright 2011 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "IMBNodeObject.h"
+
+
+@interface IMBSmartFolderNodeObject : IMBNodeObject
+
++ (NSImage *)icon;		// usable from other places for a smart folder
+
+
+@end
