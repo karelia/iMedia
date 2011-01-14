80611999fccbf1a4c46d30534fbe7b80a4ef5010
mike.abdullah@0a3f77c4-9235-0410-95f4-d138c4647c51 <mike.abdullah@0a3f77c4-9235-0410-95f4-d138c4647c51>
2011-01-14 12:00:44 +0000

    Copy .destinationDirectoryPath.
    --HG--
    extra : convert_revision : svn%3A0a3f77c4-9235-0410-95f4-d138c4647c51/trunk%401465

--- IMBObjectsPromise.h
+++ IMBObjectsPromise.h
@@ -132,7 +132,7 @@ extern NSString* kIMBPasteboardTypeObjectsPromise;
 // Optional download folder (only needed for remote files that need to be downloaded)
 // Should be preset to a reasonable default
 
-@property (retain) NSString* destinationDirectoryPath;
+@property (copy) NSString* destinationDirectoryPath;
 
 // Array of URLs referencing a local copy of a file, or in the case of e.g. link objects,
 // the URL to the web resource itself. Generally speaking, these URLs are suitable for
