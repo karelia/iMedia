8e61bce241aa4151c5cc7d4cd9512e823de576e0
Christoph Priebe <christoph@zykloid.com>
2011-02-15 14:01:42 +0000

    Added forgotten class declaration to help the compiler do its work.

--- IMBFlickrParser.h
+++ IMBFlickrParser.h
@@ -59,6 +59,7 @@
 
 //----------------------------------------------------------------------------------------------------------------------
 
+@class IMBFlickrNode;
 @class IMBLoadMoreObject;
 @class OFFlickrAPIContext;
 
