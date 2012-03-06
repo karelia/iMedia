005e68306cc45ff84b10acbb3481dbc37cc10992
Pierre Bernard <pierre.bernard@web.de>
2012-03-06 15:36:12 +0100

    Adds Lightroom 4 parsers. Lightroom 4 appears identical to version 3, but does not lock the database.

---
+++ IMBLightroom4VideoParser.h
@@ -0,0 +1,13 @@
+//
+//  IMBLightroom4VideoParser.h
+//  iMedia
+//
+//  Created by Pierre Bernard on 6/3/12.
+//  Copyright (c) 2012 Houdah Software. All rights reserved.
+//
+
+#import "IMBLightroomParser.h"
+
+@interface IMBLightroom4VideoParser : IMBLightroomParser
+
+@end
