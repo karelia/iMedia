5d0583569433485e233fd0f8e0b17c1a9ee19719
Peter Baumgartner <peter@boinx.com>
2012-08-28 11:59:59 +0200

    First draft of new controller that manages user granted access rights to file system

---
+++ IMBEntitlementsController.h
@@ -0,0 +1,99 @@
+/*
+ iMedia Browser Framework <http://karelia.com/imedia/>
+ 
+ Copyright (c) 2005-2012 by Karelia Software et al.
+ 
+ iMedia Browser is based on code originally developed by Jason Terhorst,
+ further developed for Sandvox by Greg Hulands, Dan Wood, and Terrence Talbot.
+ The new architecture for version 2.0 was developed by Peter Baumgartner.
+ Contributions have also been made by Matt Gough, Martin Wennerberg and others
+ as indicated in source files.
+ 
+ The iMedia Browser Framework is licensed under the following terms:
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in all or substantial portions of the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to permit
+ persons to whom the Software is furnished to do so, subject to the following
+ conditions:
+ 
+	Redistributions of source code must retain the original terms stated here,
+	including this list of conditions, the disclaimer noted below, and the
+	following copyright notice: Copyright (c) 2005-2012 by Karelia Software et al.
+ 
+	Redistributions in binary form must include, in an end-user-visible manner,
+	e.g., About window, Acknowledgments window, or similar, either a) the original
+	terms stated here, including this list of conditions, the disclaimer noted
+	below, and the aforementioned copyright notice, or b) the aforementioned
+	copyright notice and a link to karelia.com/imedia.
+ 
+	Neither the name of Karelia Software, nor Sandvox, nor the names of
+	contributors to iMedia Browser may be used to endorse or promote products
+	derived from the Software without prior and express written permission from
+	Karelia Software or individual contributors, as appropriate.
+ 
+ Disclaimer: THE SOFTWARE IS PROVIDED BY THE COPYRIGHT OWNER AND CONTRIBUTORS
+ "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+ AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH, THE
+ SOFTWARE OR THE USE OF, OR OTHER DEALINGS IN, THE SOFTWARE.
+*/
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+// Author: Peter Baumgartner, Jörg Jacobson
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+#pragma mark ABSTRACT
+
+// This singleton controller is responsible for letting the user grant read/write access to parts of the file system.
+// This access is persistent. The controller provides the UI, storage, and accessor methods...
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+#pragma mark 
+
+@interface IMBEntitlementsController : NSObject
+{
+	NSMutableDictionary* _bookmarks;
+}
+
+// Create singleton instance of the controller...
+
++ (IMBEntitlementsController*) sharedEntitlementsController;
+
+- (void) loadFromPrefs;
+- (void) saveToPrefs;
+
+// User inteface to grant access to part of the file system. Returns a bookmark for the folder that the user
+// actually selected or nil in case of Cancel...
+
+- (void) presentConfirmationUserInterfaceForURL:(NSURL*)inSuggestedURL;
+
+// Accessor method. Returns a security scoped bookmark, if the user has granted acccess to this part of the
+// file system. Please note that the bookmark may point to an ancestor of the specified URL. If the user
+// hasn't granted access, then nil may be returned...
+
+- (NSData*) confirmedBookmarkForURL:(NSURL*)inURL;
+
+// Helper methods...
+
+- (NSURL*) commonAncestorForURLs:(NSArray*)inURLs;								// Finds the common ancestor folder for an array of urls
+
+@end
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
