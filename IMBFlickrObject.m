72d3e79ebaec7a9f8e70c4018d71f89009f7a4b3
Mike Abdullah <me@mikeabdullah.net>
2012-11-01 20:04:42 +0000

    Revert "Switch over to kCGImagePropertyIPTCRightsUsageTerms now we're 10.6+"
    This reverts commit 9f133c521451a9a98be0d162beb73a3a5461db28.

--- IMBFlickrObject.m
+++ IMBFlickrObject.m
@@ -168,7 +168,7 @@
 	NSString *creatorAndURL = [NSString stringWithFormat:@"%@ - %@", credit, [shortWebPageURL absoluteString]];
 	NSDictionary *IPTCProperties = [NSDictionary dictionaryWithObjectsAndKeys:
 									creatorAndURL, kCGImagePropertyIPTCSource,
-									licenseDescription, kCGImagePropertyIPTCRightsUsageTerms,
+									licenseDescription, @"UsageTerms", // kCGImagePropertyIPTCRightsUsageTerms not in 10.5 headers
 									appSource, kCGImagePropertyIPTCOriginatingProgram,
 									nil];
 	NSDictionary *properties = [NSDictionary dictionaryWithObject:IPTCProperties forKey:(NSString *)kCGImagePropertyIPTCDictionary];
