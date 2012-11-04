88a0ff3c9cbc328b6bcedfed9242c509e31fd05c
Mike Abdullah <me@mikeabdullah.net>
2012-11-04 19:26:14 +0000

    Ignore custom parsers we can't make use of. User can re-add it if they need.

--- IMBParserController.m
+++ IMBParserController.m
@@ -409,18 +409,13 @@ static NSMutableDictionary* sRegisteredParserClasses = nil;
 	
 	for (NSDictionary* info in customParsers)
 	{
-		Class parserClass = NSClassFromString([info objectForKey:@"className"]);
-		NSString* mediaType = [info objectForKey:@"mediaType"];
-		IMBParser* parser = [[parserClass alloc] initWithMediaType:mediaType];
-		
-		// Restore bookmark
+		// Restore access from bookmark of mediaSource. If can't get access to the folder, it's better to just pretend we've forgotten about the folder than to try and explain we know it but need to go via an Open Panel to gain access again
         NSData *bookmark = [info objectForKey:@"bookmark"];
-        parser.bookmarkData = bookmark;
         
         NSURL *folderURL = nil;
         if (bookmark)
         {
-            folderURL = [NSURL URLByResolvingBookmarkData:bookmark options:0 relativeToURL:nil bookmarkDataIsStale:NULL error:NULL];
+            folderURL = [NSURL URLByResolvingBookmarkData:bookmark options:NSURLBookmarkResolutionWithSecurityScope relativeToURL:nil bookmarkDataIsStale:NULL error:NULL];
         }
         if (!folderURL)
         {
@@ -428,7 +423,26 @@ static NSMutableDictionary* sRegisteredParserClasses = nil;
             if (path) folderURL = [NSURL fileURLWithPath:path isDirectory:YES];
         }
         
-		parser.mediaSource = [folderURL path];
+        if ([folderURL respondsToSelector:@selector(startAccessingSecurityScopedResource)])
+        {
+            if ([folderURL startAccessingSecurityScopedResource])
+            {
+                [folderURL stopAccessingSecurityScopedResource];
+            }
+            else
+            {
+                NSLog(@"iMedia: Unable to access custom folder: %@", [folderURL path]);
+                continue;
+            }
+        }
+        
+        // Create the parser and set it up again
+		Class parserClass = NSClassFromString([info objectForKey:@"className"]);
+		NSString* mediaType = [info objectForKey:@"mediaType"];
+		IMBParser* parser = [[parserClass alloc] initWithMediaType:mediaType];
+		
+		parser.bookmarkData = bookmark;
+        parser.mediaSource = [folderURL path];
 		parser.custom = YES;
     
 		[self addCustomParser:parser forMediaType:parser.mediaType];
